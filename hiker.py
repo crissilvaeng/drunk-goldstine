cd92af9977eb4722ebd9e1e574b3900f8abca86a
Cristina Silva <crissilva.eng@gmail.com>
2021-02-27 22:14:43 +0000

    This branch is so dirty, even your mom can't clean it.

---
+++ hiker.py
@@ -0,0 +1,18 @@
+'''The starting files are unrelated to the exercise.
+
+They simply show syntax for writing and testing
+  o) a global function
+  o) an instance method
+Pick the style that best fits the exercise.
+Then delete the other one, along with this comment!
+'''
+
+
+def global_answer():
+    return 42
+
+
+class Hiker:
+
+    def instance_answer(self):
+        return global_answer()
---
+++ main.py
@@ -0,0 +1,5 @@
+from hiker import Hiker
+
+if __name__ == "__main__":
+    hiker = Hiker()
+    print(hiker.instance_answer())
